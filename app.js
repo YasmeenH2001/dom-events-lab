d4c96dd6ec0a7d6c9848f1318f3e27b3da59af4d
User Name <jamria2001@gmail.com>
2025-02-15 11:03:51 +0300

    trying

--- app.js
+++ app.js
@@ -17,20 +17,17 @@ let operator = '';
 
 
 
-
 /*----------------------------- Event Listeners -----------------------------*/
 
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
       const buttonText = event.target.innerText;
   
-      // Handle number clicks
     if (event.target.classList.contains('number')) {
         currentValue += buttonText; 
         display.innerText = currentValue;
       }
   
-      // Handle operator clicks
       if (event.target.classList.contains('operator')) {
 
         if (buttonText === '+') {
@@ -48,14 +45,15 @@ buttons.forEach((button) => {
           currentValue = '';
           operator = buttonText;
 
-        } else if (buttonText === '/') {
-          previousValue = currentValue;
-          currentValue = '';
-          operator = buttonText;
-        }
+        } 
+
+        else if (buttonText === '/') {
+            previousValue = currentValue; 
+            currentValue = ''; 
+            operator = buttonText;     
+          }
       }
   
-      // Handle equals button
       if (event.target.classList.contains('equals')) {
 
         if (operator === '+') {
@@ -69,23 +67,28 @@ buttons.forEach((button) => {
         } else if (operator === '*') {
           currentValue = (parseFloat(previousValue) * parseFloat(currentValue)).toString();
           display.innerText = currentValue;
-          
-        } else if (operator === '/') {
-          currentValue = (parseFloat(previousValue) / parseFloat(currentValue)).toString();
-          display.innerText = currentValue;
-        }
-        previousValue = '';
-        operator = '';
-      }
+
+        } 
   
+        else if (operator === '/') {
+            currentValue = (parseFloat(previousValue) / parseFloat(currentValue).toExponential(2)).toString(); 
+            display.innerText = currentValue; 
+        }
+        previousValue = ''; 
+        operator = ''; 
+    }
 
 /*-------------------------------- Functions --------------------------------*/
 
-if (buttonText === 'C') {
-    currentValue = '';
-    previousValue = '';
-    operator = '';
-    display.innerText = '0';
-  }
-});
-});
+    if (buttonText === 'C') {
+        currentValue = '';
+        previousValue = '';
+        operator = '';
+        display.innerText = '0';
+      }
+    });
+    });
+
+
+
+
