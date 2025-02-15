dc6e5fef1de420e3f2457fcd0f27f248e92eac45
User Name <jamria2001@gmail.com>
2025-02-15 10:37:20 +0300

    working on

--- app.js
+++ app.js
@@ -4,6 +4,19 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
+const buttons = document.querySelectorAll('.button');
+
+
 /*----------------------------- Event Listeners -----------------------------*/
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      // This log is for testing purposes to verify we're getting the correct value
+      console.log(event.target.innerText);
+      // Future logic to capture the button's value would go here...
+    });
+  });
+  
+
 
 /*-------------------------------- Functions --------------------------------*/
