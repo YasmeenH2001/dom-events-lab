edf1d2785fa01ee8afeb8158879d039381ed243e
User Name <jamria2001@gmail.com>
2025-02-15 10:40:23 +0300

    still working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 /*------------------------ Cached Element References ------------------------*/
 
 const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,6 +17,22 @@ buttons.forEach((button) => {
       console.log(event.target.innerText);
       // Future logic to capture the button's value would go here...
     });
+  });
+  
+  calculator.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    // You have to click a button to see this log
+    console.log(event.target.innerText);
+  
+    // Example
+    if (event.target.classList.contains('number')) {
+      // Do something with a number
+    }
+  
+    // Example
+    if (event.target.innerText === '*') {
+      // Do something with this operator
+    }
   });
   
 
